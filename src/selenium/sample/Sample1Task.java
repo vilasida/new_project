b9fe9cfb84fa994b1900e8e29bd1a02cbb8d2125
student <student@student.com>
2018-08-02 10:40:36 +0300

    Class Work

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -12,9 +12,20 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //         TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver Chromedriver = new ChromeDriver();
+
 //         go to https://kristinek.github.io/test-sample/index2.html
+        Chromedriver.get("https://kristinek.github.io/test-sample/index2.html");
 //         get title of page
-//         get URL of current page
+        System.out.println(Chromedriver.getTitle());
+//        goto https://google.com
+        Chromedriver.get("https://google.com");
+//        get URL of current page
+        System.out.println(Chromedriver.getCurrentUrl());
+//        sleep for 10 seconds
+        //Thread.sleep(10000);
 //         close browser
+        Chromedriver.close();
     }
 }
