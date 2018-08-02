67b33815170776c108e3596ae5db725e6185e6a7
student <student@student.com>
2018-08-02 16:48:56 +0300

    sample task 4

--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -16,7 +19,7 @@ public class Sample3Task {
     @Before
     public void startingTests() throws Exception {
         // from Sample 1:
-        String libWithDriversLocation =  System.getProperty("user.dir") + "\\lib\\";
+        String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         // declaration above:
         driver = new ChromeDriver();
@@ -35,25 +38,47 @@ public class Sample3Task {
     public void assertEqualsTask() throws Exception {
 //         TODO:
 //         check how many element with class "test" there are on page (5)
+        int expectedNumberOfElements = 5;
+        int actualNumberOfElements = driver.findElements(By.className("test")).size();
+        assertEquals(expectedNumberOfElements, actualNumberOfElements);
+        System.out.println("expected: " + expectedNumberOfElements + "\n actual: " + actualNumberOfElements);
 //         check that value of second button is "This is also a button"
+        String button = "This is also a button";
+        String acctualButton = driver.findElement(By.name("test")).getAttribute("value");
+        assertEquals(button, acctualButton);
+
     }
+
 
     @Test
     public void assertTrueTask() throws Exception {
 //         TODO:
-//         check that it is True that value of second button is "this is Also a Button" if you ignore Caps Locks
+//         check that it is True that value of second button is "this is Also a Button" if you ignore Caps Locks;
+        String value=driver.findElement(By.id("buttonId")).getAttribute("this is Also a Button");
+        assertTrue(value.equals("this is Also a Button"));
 //         fail with custom error message:
+          fail("ooops");
+
     }
 
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
 //        check that it is False that value of second button is "This is a button"
+       String element=driver.findElement(By.name("randomButton2")).getAttribute("value");
+       assertFalse(!element.equals("This is a button"));
     }
 
     @Test
     public void failTask() throws Exception {
 //        TODO:
 //        check that none of items with class "test" contain number 190
+       List<WebElement> listE = driver.findElements(By.className("test"));
+       for (WebElement opElements: listE){
+           System.out.println(opElements.getText());
+           assertFalse(opElements.getText().contains("190"));
+
+       }
+
     }
 }
--- src/selenium/sample/Sample4Task.java
+++ src/selenium/sample/Sample4Task.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.*;
 public class Sample4Task {
     WebDriver driver;
     String base_url = "https://kristinek.github.io/test-sample/examples/act";
+    String home_url="https://kristinek.github.io/test-sample/";
+    String home_url2="https://kristinek.github.io/test-sample/";
 
     // method which is being run before each test
     @Before
@@ -37,21 +39,43 @@ public class Sample4Task {
     public void enterNumber() throws Exception {
 //         TODO:
 //        enter a number under "Number"
+        WebElement numArea = driver.findElement(By.id("number"));
+        WebElement ResultButton = driver.findElement(By.id("result_button_number"));
+        WebElement result = driver.findElement(By.id("result_number"));
+        WebElement clear=driver.findElement(By.id("clear_result_button_number"));
+       String num1="69";
+       String num2="You entered number: ";
+        numArea.clear();
+        assertEquals(numArea.getAttribute("value"), "");
+        numArea.sendKeys(num1);
+        assertEquals(numArea.getAttribute("value"), num1);
 //        check that button is not clickable
+        assertTrue(ResultButton.isEnabled());
 //        click on "Result" button
+        ResultButton.click();
 //        check that text is displayed
+        assertTrue(numArea.isDisplayed());
 //        check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
+        assertEquals(result.getText(), num2+"\""+ num1+ "\"" );
 //        check that the button "Clear Result" is clickable now
+        assertTrue(clear.isEnabled());
 //        click on "Clear Result"
+        clear.click();
 //        check that the text is still ("You entered number: "NUMBER YOU ENTERED""), but it is not displayed
+        assertFalse(result.isDisplayed());
     }
 
     @Test
     public void clickOnLink() throws Exception {
 //         TODO:
 //        check current url is base_url
-//        click on "This is a link to Homepage"
+        assertEquals(base_url, driver.getCurrentUrl());
+//        click on "This is a link to Homepage
+        WebElement link= driver.findElement(By.id("homepage_link"));
+        link.click();
 //        check that current url is not base_url
+        assertTrue(base_url, !equals(home_url));
 //        verify that current url is homepage
+        assertEquals(home_url, home_url2);
     }
 }
