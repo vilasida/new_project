fa260b5f7dc3a1c390bff1a5dac069bdd3d7dcd2
student <student@student.com>
2018-08-02 10:42:13 +0300

    Sample Task 1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -12,9 +12,19 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //         TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver ChromeDriver = new ChromeDriver();
+
 //         go to https://kristinek.github.io/test-sample/index2.html
+        ChromeDriver.get("https://kristinek.github.io/test-sample/index2.html");
+
 //         get title of page
+        System.out.println(ChromeDriver.getTitle());
+
 //         get URL of current page
+        System.out.println(ChromeDriver.getCurrentUrl());
+
 //         close browser
+        ChromeDriver.close();
     }
 }
