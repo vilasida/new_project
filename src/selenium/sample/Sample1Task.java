5a0fcc89cb9672b928b438cb2f83ee7101a990e8
student <student@student.com>
2018-08-02 10:42:45 +0300

    sample task 1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -10,6 +10,7 @@ public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
+        System.out.printf("iuih");
 //         TODO:
 //         define driver
 //         go to https://kristinek.github.io/test-sample/index2.html
