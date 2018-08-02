a4b2423697f13c123106e630a41015538f96eab3
student <student@student.com>
2018-08-02 10:40:24 +0300

    Sample1 and Sample1Task

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -1,6 +1,5 @@
 package selenium.sample;
 
-
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,5 +15,17 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/test-sample/index2");
+
+        System.out.println(driver.getTitle());
+
+        System.out.println(driver.getCurrentUrl());
+
+//        Thread.sleep(5000);
+
+        driver.close();
     }
 }
